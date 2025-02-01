7fa76337e178487f26fce5892537403e51f937a2
John Doe <john@doe.org>
2025-02-01 13:47:48 +0000

    replace dependencies with modern ones

--- cli.js
+++ cli.js
@@ -21,36 +21,67 @@
 
 /* eslint-disable no-console */
 
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const compiler = require("./src/compiler");
 
-const version = require("./package").version;
+function parseArgs(usageInfo, ...def){
+    function processDef(...def){
+      const res = {};
+      for (const d of def) {
+        if (d.full) {
+            res['-'+d.full] = d;
+            res['--'+d.full] = d;
+        }
+        if (d.short) {
+            res['-'+d.short] = d;
+            res['--'+d.short] = d;
+        }
+      }
+      return res;
+    }
+    function usage(){
+      console.log(usageInfo)
+      process.exit(1);
+    }
+    def = processDef(...def);
+    const args = process.argv.slice(2);
+    const rest = [];
+    const data = {};
+    for (let i=0; i<args.length; i++) {
+      const a = args[i];
+      if (a==='-h' || a==='--help') usage();
+      const d = def[a];
+      if (d) {
+        if (d.type==='boolean')  data[d.full] = true
+        else data[d.full] = args[++i];
+        continue;
+      } else {
+        if (a.startsWith('-')) throw new Error('Unknown flag: '+a);
+        rest.push(a);
+      }
+    }
+    return {...data, _: rest};
+  }
 
-const argv = require("yargs")
-    .version(version)
-    .usage("circom [input source circuit file] -o [output definition circuit file]")
-    .alias("o", "output")
-    .help("h")
-    .alias("h", "help")
-    .option("verbose", {
-        alias: "v",
-        type: "boolean",
-        description: "Run with verbose logging"
-    })
-    .option("fast", {
-        alias: "f",
-        type: "boolean",
-        description: "Do not optimize constraints"
-    })
-    .epilogue(`Copyright (C) 2018  0kims association
+const argv = parseArgs(`circom [input source circuit file] -o [output definition circuit file]
+
+Options:
+  --version      Show version number                                   [boolean]
+  -h, --help     Show help                                             [boolean]
+  --verbose, -v  Run with verbose logging                              [boolean]
+  --fast, -f     Do not optimize constraints                           [boolean]
+
+Copyright (C) 2018  0kims association
     This program comes with ABSOLUTELY NO WARRANTY;
     This is free software, and you are welcome to redistribute it
     under certain conditions; see the COPYING file in the official
-    repo directory at  https://github.com/iden3/circom `)
-    .argv;
-
+    repo directory at  https://github.com/iden3/circom`, 
+    { full: 'output', short: 'o' }, 
+    { full: 'verbose', short: 'v', type: 'boolean' }, 
+    { full: 'fast', short: 'f', type: 'boolean' }
+);
 
 let inputFile;
 if (argv._.length == 0) {
